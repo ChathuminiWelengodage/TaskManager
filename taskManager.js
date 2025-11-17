77be118dece2cd8930208f737401915afc262ebd
ChathuminiWelengodage <chathuminiwelengodage@gmail.com>
2025-11-17 22:29:55 +0530

    Updated

--- taskManager.js
+++ taskManager.js
@@ -1,6 +1,17 @@
 console.log("Welcome to Task Manager!");
+console.log("                        ");
 console.log("1.List tasks");
 console.log("2.Add new task");
 console.log("3.Mark task as completed");
 console.log("4.Delete task");
 console.log("5.Exit");
+
+const readline = require("readline");
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+r1.question("Enter Your Choice:", function(choice){
+    console.log(choice);
+    r1.close();
+})

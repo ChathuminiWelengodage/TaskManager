73aac6023c0251dc9bd0d2126efa1c49d2d3901b
ChathuminiWelengodage <chathuminiwelengodage@gmail.com>
2025-11-17 21:02:27 +0530

    added new feature

---
+++ taskManager.js
@@ -0,0 +1,6 @@
+console.log("Welcome to Task Manager!");
+console.log("1.List tasks");
+console.log("2.Add new task");
+console.log("3.Mark task as completed");
+console.log("4.Delete task");
+console.log("5.Exit");
